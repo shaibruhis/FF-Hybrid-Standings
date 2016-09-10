5b21b5a45f7a9abbf9090d24d669b84db5f71209
Shai Bruhis <shaibruhis@gmail.com>
2016-09-09 23:21:15 -0700

    moved table headers into <thead> to make columns sortable

--- content_script.js
+++ content_script.js
@@ -24,15 +24,18 @@ $(document).ready(function() {
     const NUMBER_OF_COLUMNS = 12;
     const NUMBER_OF_EXISTING_COLUMNS = 6;
     const COLUMN_WIDTH = ((100-OWNER_COLUMN_WIDTH)/NUMBER_OF_COLUMNS).toString() + '%';
-    const COLUMN_HEADERS = ['Team', 'Total W', 'Total L', 'Total T', 'H2H W', 'H2H L', 'H2H T', 'Points W', 'Points L', 'Points T', 'PCT', 'GB']
+    const COLUMN_HEADERS = ['Team', 'Total W', 'Total L', 'Total T', 'H2H W', 'H2H L', 'H2H T', 'Points W', 'Points L', 'Points T', 'PCT', 'GB'];
     const LEAGUE_ID = getLeagueID();
     const SEASON_ID = getSeasonID();
+    const SCOREBOARD_URL = 'http://games.espn.com/ffl/scoreboard?leagueId='+LEAGUE_ID+'&seasonId='+SEASON_ID+'&matchupPeriodId=';
 
 
+    // make the head span across all columns (subheaders)
     function updateHeaderColumns(tableHeader) {
         $(tableHeader.children()[0]).attr('colspan', NUMBER_OF_COLUMNS.toString()); // increase width of header to account for added columns
     }
 
+    // make table correct width
     function updateTableWidth(table) {
         $(table).attr('width', '100%');
         $(table).nextAll().remove();    // get rid of weird whitespace on right side of table
@@ -76,7 +79,7 @@ $(document).ready(function() {
     }
 
 
-
+    // add text to subheaders
     function updateSubHeaderColumns(subHeader) {
         subHeaderColumns = $(subHeader).children();
         editSubHeaderColumns(subHeaderColumns);
@@ -97,10 +100,6 @@ $(document).ready(function() {
     }
 
 
-    function getPointsResults(owner) {
-        return [3,9,1];
-    }
-
     function getTotalResults(H2HResults, pointsResults) {
         returnArray = [3];
         for (var idx = 0; idx < H2HResults.length; idx++) {
@@ -111,11 +110,13 @@ $(document).ready(function() {
 
 
     function updateRows(rows) {
-        var results = getH2HResults($(rows[0]).children());
-        var numOfWeeks = getNumOfWeeks(results); // wins + losses + ties
-        for (var idx = 0; idx < rows.length; idx++) {
-            updateRow($(rows[idx]).children(), numOfWeeks);
-        }
+        var H2HResults = getH2HResults($(rows[0]).children());
+        var numOfWeeks = getNumOfWeeks(H2HResults); // wins + losses + ties
+        getPointsResults(numOfWeeks, rows, function(rows, pointsResults) {
+            for (var idx = 0; idx < rows.length; idx++) {
+                updateRow($(rows[idx]).children(), numOfWeeks, pointsResults);
+            }
+        });
     }
 
 
@@ -132,7 +133,7 @@ $(document).ready(function() {
     }
 
 
-    function updateRow(row, numOfWeeks) {
+    function updateRow(row, numOfWeeks, pointsResults) {
         // add cells
         for (var idx = NUMBER_OF_COLUMNS-NUMBER_OF_EXISTING_COLUMNS; idx < NUMBER_OF_COLUMNS; idx++) {
             var newColumn = createNewColumn("");
@@ -140,11 +141,11 @@ $(document).ready(function() {
         }
 
         owner = $(row).first().children().first().attr('title');
-
         // get and build results
-        H2HResults = getH2HResults($(row)); // H2H W | H2H L | H2H T
-        pointsResults = getPointsResults(owner); // Points W | Points L | Points T
+        var H2HResults = getH2HResults($(row)); // H2H W | H2H L | H2H T
+        pointsResults = pointsResults[owner]; // Points W | Points L | Points T
         var results = getTotalResults(H2HResults, pointsResults);
+
         results = results.concat(H2HResults, pointsResults);    // Total W | Total L | Total T | H2H W | H2H L | H2H T | Points W | Points L | Points T
         results.push(getPercentage(results, numOfWeeks));   // add % to results array
         results.push('--'); //TODO: implement GB calculation (might be better to populate and sort table and then do calculation)
@@ -154,12 +155,73 @@ $(document).ready(function() {
     }
 
 
-    function addHybridDataToTable() {
-        var tableHeader = $(TABLE_HEADER)[0];   // 2 tables on this page and we want the first one
+    function reformatTableHeader(tableHeader) {
         updateTableWidth($(tableHeader).parents('td:first'));
+        $(tableHeader).parents('tbody:first').before('<thead></thead>');
         updateHeaderColumns($(tableHeader));
         updateSubHeaderColumns($(tableHeader).next());
-        updateRows($(tableHeader).nextAll().slice(1));  // we want to skip over the subheader and update all the rows after that
+        // move the subheader into the <thead>, have to do this first because we are referencing it from tableHeader
+        jQuery($(tableHeader).next()).detach().appendTo('thead');
+        // then we can move tableHeader
+        jQuery(tableHeader).detach().prependTo('thead');
+    }
+
+
+    function addHybridDataToTable() {
+        var tableHeader = $(TABLE_HEADER)[0];   // 2 tables on this page and we want the first one
+        // get rows before we rearrange html elements
+        var rows = $(tableHeader).nextAll().slice(1)
+        reformatTableHeader($(tableHeader));
+        updateRows($(rows));  // we want to skip over the subheader and update all the rows after that
+    }
+
+
+
+    function parseHTML(HTML, pointsResults) {
+        var scoreObjects = [];      // [{owner:'owner1', score:100}, {owner:'owner2', score:97}, etc]
+
+        var scoresArray = $(HTML).find('[id^=teamscrg_]');
+        for (var idx = 0; idx < scoresArray.length; idx++) {
+            var owner = $(scoresArray[idx]).find('a').attr('title');
+            var score = $(scoresArray[idx]).find('.score').attr('title');
+            var obj = {}
+            obj['owner'] = owner;
+            obj['score'] = parseFloat(score);
+            scoreObjects.push(obj);
+        }
+
+        scoreObects = scoreObjects.sort(function(a, b) {
+            return b.score-a.score;
+        });
+
+        if (jQuery.isEmptyObject(pointsResults)) {
+            for (var idx = 0; idx < scoreObjects.length; idx++) {
+                var obj = {};
+                pointsResults[scoreObjects[idx].owner] = [0,0,0];
+            }
+        }
+        scoreObjects = scoreObjects.slice(0,scoreObjects.length/2);
+        // store into pointsResults
+        for (var idx = 0; idx < scoreObjects.length; idx++) {
+            pointsResults[scoreObjects[idx].owner][0] += 1;
+        }
+        return pointsResults;
+    }
+
+
+    function getPointsResults(numOfWeeks, rows, completionHandler) {
+        var pointsResults = {};     // {'owner1':[W,L,T], 'owner2':[W,L,T], etc}
+        var count = 0;
+
+        for (var weekNum = 1; weekNum <= numOfWeeks; weekNum++) {
+            $.get(SCOREBOARD_URL+weekNum, function(data) {
+                pointsResults = parseHTML(data, pointsResults);
+                count++;
+                if(count > numOfWeeks - 1) {
+                    completionHandler(rows, pointsResults);
+                }
+            });
+        }
     }
 
     addHybridDataToTable();
