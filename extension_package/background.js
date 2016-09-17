87ca881abe056131972764fc05f9aa8118ac61f1
Shai Bruhis <shaibruhis@gmail.com>
2016-09-16 23:27:42 -0700

    all pages work. not using chrome.storage though. calculating on each page load... :/

--- extension_package/background.js
+++ extension_package/background.js
@@ -2,26 +2,16 @@ chrome.runtime.onStartup.addListener(function() {
     chrome.storage.local.clear();   // because there is no session storage in chrome.storage
 });
 
-// chrome.tabs.onUpdated.addListener(function(integer tabId, object changeInfo, Tab tab) {
-//     if (changeInfo.status == 'complete' && changeInfo.url.match(/^(http:\/\/games\.espn\.com\/ffl\/)(leagueId=){1}/)) {
-
-//         function getSeasonID() {
-//             var seasonID = "2016";
-//             var matches = changeInfo.url.match(/seasonId=(\d+)/);
-//             if (matches) {
-//                 seasonID = matches[1];
-//             }
-//             else if ($('.games-alert-mod').children('b').text().match(/\d{4}/)) {  // some pages dont have seasonId in the URL so I check if there is the ESPN warning that I am looking at an old league
-//                 seasonID = $('.games-alert-mod').children('b').text();
-//             }
-//             return seasonID;
-//         }
-
-//         const LEAGUE_ID = changeInfo.url.match(/leagueId=(\d+)/)[1];
-//         const SEASON_ID = getSeasonID();
-
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     if (changeInfo['status'] == 'complete' && /games\.espn\.com\/.*leagueId/.test(tab.url)) {
+//         // chrome.storage.local.get(null, function(recordsObj) {
+//             // if (!Object.keys(recordsObj).length) {
+//         chrome.tabs.executeScript({file: "content_scripts/content_script_getdata.js"};
+//             // }
+//         // });
 //     }
 // });
+
 
 
 // structure of object in storage
